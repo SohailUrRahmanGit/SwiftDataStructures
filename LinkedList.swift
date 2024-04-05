3f288e00b17ccde4d878441f32e0fd3134b38928
Sohail Ur Rahman S <ssohailmails@gmail.com>
2024-04-05 12:35:44 +0530

    Push and Append Code added

--- LinkedList.swift
+++ LinkedList.swift
@@ -22,7 +22,44 @@ extension Node: CustomStringConvertible {
         guard let next = next else {
             return "\(String(describing: val))"
         }
-        return "\(val) -> \(String(describing: next.val))"
+        return "\(val) -> " + String(describing: next) + " "
     }
     
+}
+
+public struct LinkedList<T> {
+    public var head: Node<T>?
+    public var tail: Node<T>?
+    
+    public init() {}
+    
+    public var isEmpty: Bool {
+        return head == nil
+    }
+    
+    mutating public func push(_ value: T) {
+        head = Node(val: value, next: head)
+        if tail == nil {
+            tail = head
+        }
+    }
+    
+    mutating public func append(_ value: T) {
+        guard !isEmpty else {
+            push(value)
+            return
+        }
+        tail!.next = Node(val: value, next: nil)
+        tail = tail!.next
+    }
+    
+}
+
+extension LinkedList: CustomStringConvertible {
+    public var description: String {
+        guard let head = head else {
+            return "Empty List"
+        }
+        return String(describing: head)
+    }
 }
